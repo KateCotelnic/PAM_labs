10dc5547853b88e318baa36f9171365e7f91baa5
Katie Kot <55457576+KateCotelnic@users.noreply.github.com>
2021-03-12 07:35:52 +0200

    Update SecondActivity.kt

--- lab 2/java/com/example/myapp/SecondActivity.kt
+++ lab 2/java/com/example/myapp/SecondActivity.kt
@@ -8,10 +8,6 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class SecondActivity : AppCompatActivity() {
-//    companion object {
-//        const val GOT_DATA = "got_data"
-//        const val USER = "Marry"
-//    }
     companion object{
         const val USER = 0
     }
@@ -39,38 +35,6 @@ class SecondActivity : AppCompatActivity() {
 
     fun new_button(view: View) {
         val newIntent = Intent(this, ThirdActivity::class.java)
-//        newIntent.putExtra(SecondActivity.GOT_DATA, string)
         startActivityForResult(newIntent, USER)
     }
 }
-//        showGotText()
-//    }
-//
-//    fun showGotText() {
-//        val text = intent.getStringExtra(GOT_DATA)
-//        val textView: TextView = findViewById(R.id.textView) as TextView
-//        textView.setText(text)
-//    }
-//
-//    fun change(view: View){
-//        val myToast = Toast.makeText(this, "changed!", Toast.LENGTH_SHORT)
-//        val textView: TextView = findViewById(R.id.textView) as TextView
-//        val string = textView.text.toString().substringBefore(" ") + " Tom!"
-////        println("string = " + string)
-//        textView.setText(string)
-//        myToast.show()
-//    }
-//
-//    fun send(view: View){
-////        val myToast = Toast.makeText(this, "sent!", Toast.LENGTH_SHORT)
-////        myToast.show();
-//        val textView: TextView = findViewById(R.id.textView) as TextView
-//        val string = textView.text.toString()
-//        println("str to send = " + string)
-//        val answerIntent = Intent(this, MainActivity::class.java)
-//        answerIntent.putExtra(USER, string)
-//        setResult(Activity.RESULT_OK, answerIntent)
-//        finish()
-////        startActivity(newIntent)
-//    }
-//}
