07d41d4dcd401ccaf56e4437730dcc8232277375
Katie Kot <55457576+KateCotelnic@users.noreply.github.com>
2021-03-12 07:33:08 +0200

    Update MainActivity.kt

--- lab 2/java/com/example/myapp/MainActivity.kt
+++ lab 2/java/com/example/myapp/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.myapp
-//import android.R
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -38,33 +37,6 @@ class MainActivity : AppCompatActivity() {
 
     fun new_button(view: View){
         val newIntent = Intent(this, ThirdActivity::class.java)
-//        newIntent.putExtra(SecondActivity.GOT_DATA, string)
         startActivityForResult(newIntent, USER)
-//        startActivity(newIntent)
     }
-//
-//    fun send(view: View){
-////        val myToast = Toast.makeText(this, "sent!", Toast.LENGTH_SHORT)
-////        myToast.show();
-//        val textView: TextView = findViewById(R.id.textView) as TextView
-//        val string = textView.text.toString()
-//        val newIntent = Intent(this, SecondActivity::class.java)
-//        newIntent.putExtra(SecondActivity.GOT_DATA, string)
-////        startActivity(newIntent)
-//        startActivityForResult(newIntent, USER)
-//    }
-//
-//    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//        val textView = findViewById<View>(R.id.textView) as TextView
-//        if (requestCode == USER) {
-//            if (resultCode == Activity.RESULT_OK) {
-//                val string = data?.getStringExtra(SecondActivity.USER)
-//                println(string)
-//                textView.text = string
-//            } else {
-//                textView.text = ""
-//            }
-//        }
-//    }
 }
